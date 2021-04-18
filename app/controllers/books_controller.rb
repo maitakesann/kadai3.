d8b9d46c5828edd74bfadbb81672cb4b80cfb05d
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-18 11:39:14 +0000

    メンターさん質問後

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -13,5 +17,12 @@ class BooksController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  get 'homes/index'
+  
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show]
 end
