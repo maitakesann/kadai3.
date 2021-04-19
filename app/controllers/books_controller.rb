8da2eb19b1bc5c3272aa37453a213deeac3fb3e2
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-19 10:49:27 +0000

    edit Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
