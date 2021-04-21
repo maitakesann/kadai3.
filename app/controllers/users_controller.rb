ce4148066c453a1f8a4761ca4807114f3e16b784
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-21 01:13:34 +0000

    user_showとbook＿show連結

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def show
+  def index
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+    # @user = User.find(params[:id])
   end
+
+  def show
+    @user = User.find(params[:id])
+    @books =@user.books
+    
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introdacution, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  # get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'homes/index'
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show, :edit, :update]
 end
