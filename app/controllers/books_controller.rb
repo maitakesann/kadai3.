49b94e23864f962ef29939c944b2a720e05e9bc7
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-21 04:44:25 +0000

    image追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,12 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    
+
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books =@user.books
-    
+
 
   end
 
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introdacution, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
