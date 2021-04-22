2fb0c40e7d7b3687b8f60dc57eee18d88ec59077
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-22 12:28:20 +0000

    books validates設定後

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true
-  
+  validates :body, presence: true, length: { maximum: 200 }
+  # before_action :ensure_user, only: [:edit, :update, :destroy]
+
 end
