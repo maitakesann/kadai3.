529c4c2338b3d4b516563ae32c164811a14250b0
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-22 06:39:49 +0000

    フラッシュメッセージ下3つ終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,16 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+    redirect_to book_path(@book), notice: "You have created book successfully."
+    end
   end
 
   def index
     @book = Book.new
     @books = Book.all
     @user = current_user
-    
+
   end
 
   def show
@@ -26,8 +27,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice:"You have updated book successfully."
+    end
   end
 
   def destroy
