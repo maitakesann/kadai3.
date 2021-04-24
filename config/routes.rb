bbf7ce9f0afb2d3f851e8bb3976563eea0c1ec67
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-24 08:00:03 +0000

    adout 修正後

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # get 'users/show'
   devise_for :users
-  root to: 'homes#top'
-  get 'homes/index'
-  get "homes/about" => "homes#about" , as: "about"
+  root to: 'home#top'
+  get "home/about" => "home#about" , as: "about"
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
