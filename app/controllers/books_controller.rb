d6d7b120743d9321e4f1ec26e727a0aa6242ad55
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-24 09:54:56 +0000

    [redirect to の確認　メンタターさん質問後]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+before_action :ensure_correct_user, only:[:edit]
+
   def create
     @user = current_user
     @book = Book.new(book_params)
@@ -55,6 +57,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+     redirect_to books_path
+    end
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :ensure_correct_user, only:[:edit]
+
+
   def index
     @users = User.all
     @book = Book.new
@@ -44,6 +48,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+    redirect_to user_path(current_user)
+    end
   end
 
 end
